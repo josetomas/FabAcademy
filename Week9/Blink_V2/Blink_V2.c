fcd92d57b2e96ed67a1bcc06ca048749c3d357ba
Jose Tomas <jose.tomas@fablab.usm.cl>
2019-03-19 10:04:54 -0400

    Makefile

--- Week9/Blink_V2/Blink_V2.c
+++ Week9/Blink_V2/Blink_V2.c
@@ -1,21 +1,22 @@
 #include <avr/io.h>
 #include <avr/delay.h>
+#include <util/delay.h>
 int main (void)
 {
-DDRA = DDRA | B11110111;
+DDRA = DDRA | 11110111;
 DDRB = 0xFF;
 while(1) {
 if (!(PINA & (1<<PA3))) {
   PORTB |=(1<<PB2); //PORTx |=(the value<<number positions)
-  _delay_ms (250); 
+  _delay_ms (1000); 
   PORTB &= ~(1<<PB2); 
-  _delay_ms (250);
+  _delay_ms (1000);
 }
   else {
     PORTA |= (1 << PA2);
-  _delay_ms (250); 
+  _delay_ms (100); 
   PORTA &= ~(1<<PA2); 
-  _delay_ms (250);
+  _delay_ms (100);
   }
 };
 }
