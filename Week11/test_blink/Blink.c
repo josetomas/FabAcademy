01912affded545127886f0d5d25c7b7bc825b446
Jose Tomas <jose.tomas@fablab.usm.cl>
2019-05-04 22:29:49 -0400

    weas bla bla

--- Week11/test_blink/Blink.c
+++ Week11/test_blink/Blink.c
@@ -1,14 +1,13 @@
 #include <avr/io.h>
-#include <avr/delay.h>
 #include <util/delay.h>
 int main (void)
 {
-DDRA = DDRA | 0b11110111;
+DDRA = 0xFF;
 DDRB = 0xFF;
 while(1) {
-  PORTB |=(1<<PB0); //PORTx |=(the value<<number positions)
+  PORTA |=(1<<PA7); //PORTx |=(the value<<number positions)
   _delay_ms (100); 
-  PORTB &= ~(1<<PB0); 
+  PORTA &= ~(1<<PA7); 
   _delay_ms (100);
 }
 }
--- Week9/Blink_V2/Blink_V2.c
+++ Week9/Blink_V2/Blink_V2.c
@@ -6,17 +6,17 @@ int main (void)
 DDRA = DDRA | 0b11110111;
 DDRB = 0xFF;
 while(1) {
-if (!(PINA & (1<<PA3))) {
-  PORTB |=(1<<PB2); //PORTx |=(the value<<number positions)
+//if (!(PINA & (1<<PA3))) {
+  PORTB |=(1<<PB0); //PORTx |=(the value<<number positions)
   _delay_ms (100); 
-  PORTB &= ~(1<<PB2); 
+  PORTB &= ~(1<<PB0); 
   _delay_ms (100);
-}
-  else {
+//}
+  /*else {
     PORTA |= (1 << PA2);
   _delay_ms (10); 
   PORTA &= ~(1<<PA2); 
   _delay_ms (10);
-  }
+  }*/
 };
 }
