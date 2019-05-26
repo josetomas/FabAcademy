6e238bd61a3076f12be9c68681b9af2f01d43c61
Jose Tomas <jose.tomas@fablab.usm.cl>
2019-05-25 20:37:24 -0400

    fail :(

--- Week14/Codigos/Master/Master.ino
+++ Week14/Codigos/Master/Master.ino
@@ -1,6 +1,4 @@
 #include <SoftwareSerial.h>
-#include <TinyWireM.h>
-#include <USI_TWI_Master.h>
 #define rxPin 0
 #define txPin 1
 
@@ -9,16 +7,8 @@ SoftwareSerial mySerial(rxPin,txPin);
 
 void setup() {
   mySerial.begin(9600);
-  //Serial.begin(9600);
-  TinyWireM.begin();
-  delay (300);
 }
 
 void loop() {
-  mySerial.println("networking week");
-  /*TinyWireM.requestFrom(8,1);
-  while (TinyWireM.available()) {
-    int n = TinyWireM.receive();
-    mySerial.println(n);
-  }*/
+  mySerial.println("Holi! :*");
 }
