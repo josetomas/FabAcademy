ef460b6fe750feff2b068488de5ba3da51bb4e9d
José Tomás Domínguez Fuenzalida <jose.tomas@fablab.usm.cl>
2019-06-09 13:41:41 -1200

    Codigos G

--- Material Util/Serial_Test/Serial_Test.ino
+++ Material Util/Serial_Test/Serial_Test.ino
@@ -4,21 +4,14 @@
 
 #define rxPin 0
 #define txPin 1
-int pot=2;
-int motor1=2;
-int motor2=3;
 
 
 SoftwareSerial mySerial( rxPin,txPin);
 void setup() {   
   mySerial.begin(9600);
-  //pinMode(motor1,OUTPUT); 
-  pinMode(motor2,OUTPUT); 
   
 }
 
 void loop() {
-  //digitalWrite(motor1,HIGH);
-  digitalWrite(motor2,HIGH); 
-  delay(1);
+  mySerial.println(":*");
 }
